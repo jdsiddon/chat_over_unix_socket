3fba0fe031184baeada9fec8fbf81f6a389519f6
justin <justin@delaris.com>
2016-05-01 15:17:28 -0400

    Added error file comments.

--- error.c
+++ error.c
@@ -1,3 +1,19 @@
+/**************************************************
+** File: error.c
+** Author: Justin Siddon
+** Description: This file provides a standard error interface for server and clients
+**   to print and return errors, based off error function: http://www.linuxhowtos.org/C_C++/socket.htm.
+**************************************************/
+
+
+/**************************************************
+** Function: error
+** Description: Prints error to stdout and exits the program.
+** Parameters:
+**  char msg - Error message
+**  int value - Error value, usually 1.
+** Returns: Nothing.
+**************************************************/
 void error(const char *msg, int value) {
   perror(msg);
   exit(value);
